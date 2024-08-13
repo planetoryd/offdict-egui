d751d3ab841628da5fc6995bba156a91aef3aced
plein <68096727+planetoryd@users.noreply.github.com>
2024-08-13 23:47:04 +0800

    stuff

--- offdict-egui/src/main.rs
+++ offdict-egui/src/main.rs
@@ -1,8 +1,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
 use eframe::egui;
+use egui_plotter::{
+    plotters::{
+        chart::ChartBuilder,
+        prelude::IntoDrawingArea,
+        style::{full_palette::WHITE, Color},
+    },
+    EguiBackend,
+};
 
-fn main() -> Result<(), eframe::Error> {
+fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
@@ -15,7 +24,7 @@ fn main() -> Result<(), eframe::Error> {
             // This gives us image support:
             egui_extras::install_image_loaders(&cc.egui_ctx);
 
-            Box::<MyApp>::default()
+            Ok(Box::<MyApp>::default())
         }),
     )
 }
@@ -37,17 +46,17 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
+            ui.heading("performance");
+            egui::Frame::default().show(ui, |ui| {
+                let r = EguiBackend::new(ui).into_drawing_area();
+                let mut cb = ChartBuilder::on(&r);
+                let mut cx = cb.build_cartesian_2d(0..590, 0..59).unwrap();
+                cx.configure_mesh()
+                    .light_line_style(WHITE.mix(0.005))
+                    .bold_line_style(WHITE.mix(0.01))
+                    .draw()
+                    .unwrap();
             });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Hide").clicked() {
-                
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
         });
     }
 }
